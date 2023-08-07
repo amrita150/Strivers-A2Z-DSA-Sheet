e4fc980702de10836351cb39c36ae5a87946f477
amrita150 <amritakataria515@gmail.com>
2023-08-07 21:36:43 +0530

    Remove nth node from end of LL

---
+++ Linked List/Medium Problems of LL/RemoveNthNode.java
@@ -0,0 +1,55 @@
+public class  RemoveNthNode{
+    static class Node {
+        public int data;
+        public Node next;
+   
+        Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void remove(Node head , int n ){
+        int size = 1;
+        Node current = head;
+        while (current != null){
+            current = current.next;
+            size++;        
+        }
+        if(n==size){
+            head = head.next;
+            return;
+        }
+        int i = 1;
+        Node prev = head;
+        int toFind = size - n;
+        if(i<toFind){
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+    }
+
+    public static void printLinked(Node startNode) {
+        Node current = startNode;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        int n = 3;
+        printLinked(head);
+
+        remove(head , n );
+        printLinked(head);    
+    }
+}
