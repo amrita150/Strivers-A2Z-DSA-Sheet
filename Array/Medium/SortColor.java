fd8d528aa0e110f7aa79ac943e21b30521ae075c
amrita150 <amritakataria515@gmail.com>
2023-08-09 23:41:38 +0530

    deleted some files

--- Array/Medium/SortColor.java
+++ Array/Medium/SortColor.java
@@ -1,34 +1,62 @@
 //also know as sort an array with 0's , 1's and 2's
 
 public class SortColor {
-    public static void sortColors(int[] arr) {
-        // using count sort
-        int zero = 0, two = 0, one = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                zero++;
-            } else if (arr[i] == 1) {
-                one++;
-            } else {
-                two++;
-            }
-        }
+    // public static void sortColors(int[] arr) {
+    //     // using count sort
+    //     int zero = 0, two = 0, one = 0;
+    //     for (int i = 0; i < arr.length; i++) {
+    //         if (arr[i] == 0) {
+    //             zero++;
+    //         } else if (arr[i] == 1) {
+    //             one++;
+    //         } else {
+    //             two++;
+    //         }
+    //     }
 
-        int i = 0;
-        while (zero-- != 0) {
-            arr[i] = 0;
-            i++;
+    //     int i = 0;
+    //     while (zero-- != 0) {
+    //         arr[i] = 0;
+    //         i++;
+    //     }
+    //     while (one-- != 0) {
+    //         arr[i] = 1;
+    //         i++;
+    //     }
+    //     while (two-- != 0) {
+    //         arr[i] = 2;
+    //         i++;
+    //     }
+    // }
+
+    //using in-Place algo also known as dutch national flag algo
+    public static void sortColors(int[] arr){
+        int left = 0 , right = arr.length-1 , mid = 0;
+        //the primary goal is to move zero to the left , one to the middle
+        //and two to the right
+        while(mid<=right){
+        if(arr[mid]==0){
+            int temp = arr[mid];
+            arr[mid] = arr[left];
+            arr[left] = temp;
+            mid++;
+            left++;
         }
-        while (one-- != 0) {
-            arr[i] = 1;
-            i++;
+        else if(arr[mid]==1){
+            int temp = arr[mid];
+            arr[mid] = arr[left];
+            arr[left] = temp;
+            mid++;
         }
-        while (two-- != 0) {
-            arr[i] = 2;
-            i++;
+        else{
+           int temp = arr[mid];
+            arr[mid] = arr[right];
+            arr[right] = temp; 
+            right--;
         }
     }
 
+    }
     public static void main(String args[]) {
 
         int arr[] = {2,0,2,1,1,0};
