5eef6322a671d9410c017c4ab3ac3187437920e5
amrita150 <amritakataria515@gmail.com>
2023-08-10 00:02:17 +0530

    deleted some files

--- Array/Basic/MoveZero.java
+++ Array/Basic/MoveZero.java
@@ -30,7 +30,7 @@ public class MoveZero {
 
       moveToEnd(arr);
 
-      System.out.println("after after this: ");
+      System.out.println("array after this: ");
       for(int i = 0 ; i< arr.length ; i++)
         {
            System.out.print(arr[i]+" ");
