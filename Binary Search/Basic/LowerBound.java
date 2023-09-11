98e3a729c5ffa18d9a39566fb981bd0bbb1605e4
amrita150 <amritakataria515@gmail.com>
2023-09-12 01:04:27 +0530

    Lower Bound

---
+++ Binary Search/Basic/LowerBound.java
@@ -0,0 +1,25 @@
+public class LowerBound {
+    public static int lowerBound(int []arr, int x){
+        // Write your code here.
+        int left = 0;
+        int right = arr.length -1 ;
+        int ans = arr.length;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(arr[mid] >= x){
+                ans = mid;
+                right = mid - 1;
+            } else{
+                left = mid + 1;
+            }
+    }
+
+    return ans;
+}
+
+public static void main(String[] args) {
+    int arr[] = {1,2,3,6,12,34,43};
+    int target = 3;
+    System.out.println(lowerBound(arr,target));
+ }
+}
