b97c67e81a5bfd371ff53726966074a9f5349b2b
amrita150 <amritakataria515@gmail.com>
2023-09-24 00:12:38 +0530

    all traversals in one traversal

--- Binary Tree/Traversals/AllTraversalInOne.java
+++ Binary Tree/Traversals/AllTraversalInOne.java
@@ -1,26 +1,14 @@
 import java.util.*;
 
-class TreeNode {
+class TreeNode{
     int data;
     TreeNode left;
     TreeNode right;
-
-    TreeNode() {
-        this.data = 0;
-        this.left = null;
-        this.right = null;
-    }
 
     TreeNode(int data) {
         this.data = data;
         this.left = null;
         this.right = null;
-    }
-
-    TreeNode(int data, TreeNode left, TreeNode right) {
-        this.data = data;
-        this.left = left;
-        this.right = right;
     }
 }
 
